de13773d1f0730a42a5642b423cbb677e5b205c5
Gogs <gogs@fake.local>
2018-12-27 12:29:49 -0600

    Version en uso

---
+++ SHA384/Hasher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace GetHash
+{
+    class Hasher
+    {
+        private string result;
+        private readonly string path;
+
+        internal string Result
+        {
+            get
+            {
+                hash();
+                return result;
+            }
+
+            set
+            {
+                result = value;
+            }
+        }
+
+        internal Hasher(string path)
+        {
+            this.path = path;
+        }
+
+        private void hash()
+        {
+            try
+            {
+                using (var sha = SHA384.Create())
+                {
+                    using (var file = File.OpenRead(path))
+                    {
+                        Result = Convert.ToBase64String(sha.ComputeHash(file));
+                    }
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SHA384/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+                Console.WriteLine(new Hasher(args[0]).Result);
+        }
+    }
+}
